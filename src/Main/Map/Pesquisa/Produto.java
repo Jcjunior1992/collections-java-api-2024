da2b024abeec481dd2b1f316abe7e8857eb66863
Jcjunior1992 <jcjunior1992@gmail.com>
2024-05-06 22:45:03 -0300

    v4

---
+++ src/Main/Map/Pesquisa/Produto.java
@@ -0,0 +1,15 @@
+package Main.Map.Pesquisa;
+
+public class Produto {
+
+    private String produto;
+    private double preco;
+    private int quantidade;
+
+    public Produto(String produto, double preco, int quantidade) {
+        this.produto = produto;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+}
